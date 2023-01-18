b1f12354a91df7fad80ce77f39a2cfc8ebec8b77
Victor Zhou <vicode0117@outlook.com>
2023-01-18 17:35:56 +0800

    feat(hello): use greetings module Hello and Pasue function; import log package

--- hello/hello.go
+++ hello/hello.go
@@ -1,14 +1,39 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"log"
 
-import "rsc.io/quote"
-
-import "example.com/greetings"
+	"example.com/greetings"
+	"rsc.io/quote"
+)
 
 func main() {
+	// Set properties of the predefined Logger, including
+	// the log entry prefix and a flag to disable printing
+	// the time, source file, and line number.
+	log.SetPrefix("greetings: ")
+	log.SetFlags(0)
+
+	// basic
 	fmt.Println("Hello, World!")
-    fmt.Println(quote.Go())
-    message := greetings.Hello("Victor")
-    fmt.Println(message)
+
+	// quote module
+	fmt.Println(quote.Go())
+
+	// Request a greeting message.
+	message, err := greetings.Hello("test")
+
+	// error
+	// message, err := greetings.Hello("")
+	// If an error was returned, print it to the console and
+	// exit the program.
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	// If no error was returned, print the returned message
+	// to the console
+	fmt.Println(message)
+	greetings.Pause()
 }
